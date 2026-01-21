e7c0a9a3e3486f5324c95823541cef7f62f99ec6
flyingmachine001 <upkaarmalik@gmail.com>
2026-01-21 15:05:37 +0530

    OK

---
+++ Day13Exercise/Day13Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day13Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calling the method without any parameters
+            PrintLine();
+            PrintLine(ch:'$');  // named parameters
+            PrintLine(60, '+');
+        }
+        static void PrintLine(int num = 40, char ch = '-')
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+    }
+}
