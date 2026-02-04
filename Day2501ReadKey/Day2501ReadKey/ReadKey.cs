b435db9544175e5f8eb3f1150e603e2618d8cbf1
flyingmachine001 <upkaarmalik@gmail.com>
2026-02-04 14:58:09 +0530

    readkey done

---
+++ Day2501ReadKey/Day2501ReadKey/ReadKey.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Day2501ReadKey
+{
+    internal class ReadKey
+    {
+        static void Main(string[] args)
+        {
+            string pin = "";
+            int length = 4;
+
+            Console.Write("Enter 4-digit PIN: ");
+
+            while (pin.Length < length)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                
+                if (char.IsDigit(key.KeyChar))
+                {
+                    pin += key.KeyChar;
+                    Console.Write("*");
+                }
+                
+                else if (key.Key == ConsoleKey.Backspace && pin.Length > 0)
+                {
+                    pin = pin.Remove(pin.Length - 1);
+
+                    Console.Write("\b \b"); // delete last *
+                }
+            }
+
+            Console.WriteLine("\n\nEntered PIN: " + pin);
+        }
+    }
+}
