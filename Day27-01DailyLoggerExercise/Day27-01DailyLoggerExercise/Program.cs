e61d112b054a1ee966dea73f3e273b66e509eced
flyingmachine001 <upkaarmalik@gmail.com>
2026-02-06 11:47:28 +0530

    OK

---
+++ Day27-01DailyLoggerExercise/Day27-01DailyLoggerExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace Day27_01DailyLoggerExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string file = @"../../../file1.txt";
+            Console.WriteLine("Write your content: ");
+            string input = Console.ReadLine();
+
+            using (StreamWriter sw = new StreamWriter(file, true))
+            {
+                sw.WriteLine(input);
+                sw.WriteLine();
+            }
+
+            Console.WriteLine("Your reflection have been saved");
+
+
+        }
+    }
+}
