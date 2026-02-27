e9ef457948dcbb1fc9064abfe99eb5b5d673bfc7
Upkaar <Upkaar@DESKTOP-6EEBG0R>
2026-02-27 15:01:39 +0530

    ok

---
+++ Exercises/Day 43 01 Exercise Financial Portfolio/Day 43 01 Exercise Financial Portfolio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_43_01_Exercise_Financial_Portfolio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
