7dbfd509ba14a979d507b58d098e927da1b25b6c
Upkaar <Upkaar@DESKTOP-6EEBG0R>
2026-03-13 11:27:22 +0530

    done

--- Exercises/Day 55 01 Exercise MVC Models/Day 55 01 Exercise MVC Models/Controllers/HomeController.cs
+++ Exercises/Day 55 01 Exercise MVC Models/Day 55 01 Exercise MVC Models/Controllers/HomeController.cs
@@ -6,6 +6,12 @@ namespace Day_55_01_Exercise_MVC_Models.Controllers
 {
     public class HomeController : Controller
     {
+        List<Employee> employees = new List<Employee>()
+            {
+                new Employee{Id=1, Name="Rahul",Position="Developer", Salary=5000},
+                new Employee{Id=2,Name="Lochan", Position="Designer", Salary=3000},
+                new Employee{Id=3,Name="Jerry", Position="Manager",Salary=7000}
+            };
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -15,12 +21,7 @@ namespace Day_55_01_Exercise_MVC_Models.Controllers
 
         public IActionResult Index()
         {
-            List<Employee> employees = new List<Employee>()
-            {
-                new Employee{Id=1, Name="Rahul",Position="Developer", Salary=5000},
-                new Employee{Id=2,Name="Lochan", Position="Designer", Salary=3000},
-                new Employee{Id=3,Name="Jerry", Position="Manager",Salary=7000}
-            };
+            
             ViewBag.Announcement = "Company meeting at 4 PM";
             ViewData["DepartmentName"] = "IT Department";
             ViewData["ServerStatus"] = false;
